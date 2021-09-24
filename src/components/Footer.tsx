25101ed99301c6a374cfe853df5e5b37f07030d7
Lakshitha Madushan <laksithamadushan@gmail.com>
2021-09-25 01:34:46 +0530

    Added basic components.

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            test footer
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header>
+            Test header
+        </header>
+    )
+}
+
+export default Header
